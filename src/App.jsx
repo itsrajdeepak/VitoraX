49a310cd274e9c03ce0eeb2c1644c0f26002827f
itsrajdeepak <4udepak@gmail.com>
2025-07-27 16:30:57 +0530

    Fix: case-sensitive import for Navbar in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import HeroSection from './components/HeroSection'
 import Feature from './components/Feature'
 import WorkFlow  from './components/WorkFlow'
